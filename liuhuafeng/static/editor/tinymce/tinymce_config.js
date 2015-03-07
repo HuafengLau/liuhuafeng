40e343cf7d0716c2da2e06a5007bd6cbafdccecb
HuafengLau <727860046@qq.com>
2015-03-07 19:10:14 +0800

    add link and fontsize function to tineMEC
    rt

--- liuhuafeng/static/editor/tinymce/tinymce_config.js
+++ liuhuafeng/static/editor/tinymce/tinymce_config.js
@@ -4,5 +4,7 @@ tinyMCE.init({
     language:'zh_CN',
     width : 800,
     height : 600,
-    plugins: "code,hr"
+    plugins: "code,hr,link",
+    toolbar:"fontsizeselect | fontselect | link | unlink",
+    fontsize_formats: "8pt 10pt 12pt 13pt 14pt 18pt 24pt 36pt"
 });
