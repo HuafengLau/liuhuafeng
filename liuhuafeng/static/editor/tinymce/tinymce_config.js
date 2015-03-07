6f42d0fcf1915582b75f80e55488064bb5bbd827
HuafengLau <727860046@qq.com>
2015-03-07 20:18:38 +0800

    change pt to px in TinyMCE

--- liuhuafeng/static/editor/tinymce/tinymce_config.js
+++ liuhuafeng/static/editor/tinymce/tinymce_config.js
@@ -6,5 +6,5 @@ tinyMCE.init({
     height : 600,
     plugins: "code,hr,link",
     toolbar:"fontsizeselect | fontselect | link | unlink",
-    fontsize_formats: "8pt 10pt 12pt 13pt 14pt 18pt 24pt 36pt"
+    fontsize_formats: "8px 10px 12px 13px 14px 18px 24px 36px"
 });
