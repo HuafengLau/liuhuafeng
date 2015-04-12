2d755ba44aa26150a3bbc25400b5804b65d903a1
HuafengLau <727860046@qq.com>
2015-04-12 17:58:16 +0800

    add table to tinymce
    rt

--- liuhuafeng/static/editor/tinymce/tinymce_config.js
+++ liuhuafeng/static/editor/tinymce/tinymce_config.js
@@ -4,7 +4,7 @@ tinyMCE.init({
     language:'zh_CN',
     width : 800,
     height : 600,
-    plugins: "code,hr,link",
-    toolbar:"fontsizeselect | fontselect | link | unlink",
+    plugins: "code,hr,link,table",
+    toolbar:"fontsizeselect | fontselect | link | unlink | inserttable",
     fontsize_formats: "8px 10px 12px 13px 14px 18px 24px 36px"
 });
