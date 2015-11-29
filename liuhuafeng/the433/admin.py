95ccf7beb43c2c7159d03a7b65576011912ed98f
HuafengLau <HuafengLau@icloud.com>
2015-11-29 09:01:14 +0800

    update20151129
    add most models, userAddFund, webGetFundInfo, webGetFundNet

--- liuhuafeng/the433/admin.py
+++ liuhuafeng/the433/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
 # Register your models here.
+from the433.models import *
+
+class PassPortAdmin(admin.ModelAdmin):
+    """docstring for PassPortAdmin"""
+    list_display = ('phone','nicName')
+    list_filter = ('phone','nicName')
+    ordering = ('phone',)
+
+admin.site.register(PassPort, PassPortAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    """docstring for ProfileAdmin"""
+    list_display = ('passPort','lastActive')
+    list_filter = ('passPort',)
+    ordering = ('passPort','lastActive')
+
+admin.site.register(Profile, ProfileAdmin)
+
+
+class FundAdmin(admin.ModelAdmin):
+    """docstring for FundAdmin"""
+    list_display = ('code','name','types','cx3years','cx5years','cxCode')
+    list_filter = ('code','name','types','cx3years','cx5years')
+    ordering = ('code','types','cx3years','cx5years')
+
+admin.site.register(Fund, FundAdmin)
+
+class FundNetAdmin(admin.ModelAdmin):
+    """docstring for FundNetAdmin"""
+    list_display = ('fund','date','net','yields')
+    list_filter = ('fund','date')
+    ordering = ('fund','date','net','yields')
+
+admin.site.register(FundNet, FundNetAdmin)
+
+class UserFundShareAdmin(admin.ModelAdmin):
+    """docstring for UserFundShareAdmin"""
+    list_display = ('passPort','fund','share')
+    list_filter = ('passPort','fund')
+    ordering = ('passPort','fund','share')
+
+admin.site.register(UserFundShare, UserFundShareAdmin)
+
+class UserFundProfitAdmin(admin.ModelAdmin):
+    """docstring for UserFundProfitAdmin"""
+    list_display = ('passPort','fund','date','profit','totalAmount')
+    list_filter = ('passPort','date')
+    ordering = ('passPort','fund','date')
+
+admin.site.register(UserFundProfit, UserFundProfitAdmin)
+
+class UserFundOldProfitAdmin(admin.ModelAdmin):
+    """docstring for UserFundOldProfitAdmin"""
+    list_display = ('passPort','fund','year','profit')
+    list_filter = ('passPort','fund','year')
+    ordering = ('passPort','fund','year')
+
+admin.site.register(UserFundOldProfit, UserFundOldProfitAdmin)
--- liuhuafeng/the433/urls.py
+++ liuhuafeng/the433/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('the433.views',
-    url(r'^user/register/$', 'register', name='register'),
-    #url(r'^user/login/$', 'login', name='login'),
+    url(r'^passPort/register/$', 'register', name='register'),
+    url(r'^passPort/login/$', 'login', name='login'),
+    url(r'^fund/useraddfund/$', 'userAddFund', name='userAddFund'),
 )
